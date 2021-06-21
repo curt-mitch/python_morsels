23fd47ffc91c5db143e48274ec350ce9d06f8c4c
Curtis Mitchell <curtis.l.mitchell@nasa.gov>
2021-06-21 16:02:58 -0700

    Passing basic tests

--- easydict/easydict.py
+++ easydict/easydict.py
@@ -1,10 +1,10 @@
 class EasyDict(object):
-  def __init__(self, _dict=None):
-    if _dict is not None:
-      self.__dict__.update(_dict)
-      for key, value in _dict.items():
-        self[str(key)] = value
-  def __setattr__(self, key, value):
-    self[key] = value
-  def __getattr__(self, key):
-    return self[key]
+    def __init__(self, _dict=None):
+        if _dict is not None:
+            self.__dict__.update(_dict)
+
+    def __setitem__(self, key, value):
+        self.__dict__[key] = value
+
+    def __getitem__(self, key):
+        return self.__dict__[key]
